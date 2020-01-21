6e4d9cf4db9151b52f9e3691a35fdc70ef53c18d
Kartik Bhatia <37398223+KnightKnight27@users.noreply.github.com>
2020-01-21 21:52:37 +0530

    Create EDITDISTANCERECURSIVE.cpp

---
+++ EDITDISTANCERECURSIVE.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Min(int A,int B)
+{
+	return A<B?A:B;
+}
+int RECUSIVEEDITDISTANCE(string A,string B,int m,int n)
+{
+	if (m==0)
+		return n;
+	if (n==0)
+		return m;
+	if(A[m]==B[n])
+		return RECUSIVEEDITDISTANCE(A,B,m-1,n-1);
+	return 1+(Min(Min(RECUSIVEEDITDISTANCE(A,B,m-1,n-1),RECUSIVEEDITDISTANCE(A,B,m-1,n)),RECUSIVEEDITDISTANCE(A,B,m,n-1)));
+
+}
+int main()
+{
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	string A;
+	string B;
+	cin>>A;
+	cin>>B;
+	int m=A.size();
+	int n=B.size();
+	cout<<RECUSIVEEDITDISTANCE(A,B,m-1,n-1);
+}
