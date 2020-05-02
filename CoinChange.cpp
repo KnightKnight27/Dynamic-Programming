878919cab9394bf18b84bcf54faa9ec5f621c9db
Kartik Bhatia <37398223+KnightKnight27@users.noreply.github.com>
2020-05-03 00:33:17 +0530

    Create CoinChange.cpp

---
+++ CoinChange.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+	    int N;
+	    cin>>N;
+	    int arr[N];
+	    sort(arr,arr+N);
+    	for(auto &it:arr)
+    	{
+    	    cin>>it;
+    	}
+	    int M;
+	    cin>>M;
+	    int dp[M+1]={0};
+	    dp[0]=1;
+	    for(int i=0;i<N;i++)
+	    {
+	        for(int j=1;j<=M;j++)
+	        {
+	            if(arr[i]<=j)
+	            {
+	                dp[j]+=dp[j-arr[i]];
+	            }
+	        }
+	    }
+	    cout<<dp[M]<<endl;
+    }
+}
